97986a35de1efe794978397965dc055bfe4aa8a5
Marcel Rasbach <marcel.rasbach@d-fine.com>
2024-03-28 15:38:48 +0100

    datasets per framework reporting period and signed up users

--- datatest_per_framework_script.py
+++ datatest_per_framework_script.py
@@ -1,12 +1,26 @@
 from requests.auth import HTTPBasicAuth
 import requests
 from config import *
+from collections import Counter
 
-frameworks = {'sme','sfdr','p2p','lksg','heimathafen','eutaxonomy-non-financials','esg-questionnaire'}
-headers = {"Authorization": "Bearer "+PERSONAL_USER_API_KEY}
 
+def extractNumnberOfDatasetsPerReportingPeriod(response: list):
+	accessKey = 'reportingPeriod'
+	reportingPeriods = []
+	for entry in response:
+		reportingPeriods.append(entry[accessKey])
+	return dict(Counter(reportingPeriods))
+
+
+frameworks = {'sme', 'sfdr', 'p2p', 'lksg', 'heimathafen', 'eutaxonomy-non-financials', 'esg-questionnaire',
+			  'eutaxonomy-financials'}
+headers = {"Authorization": "Bearer " + PERSONAL_USER_API_KEY}
+datasetCountPerFramework = {}
 for framework in frameworks:
-	url='https://dataland.com/api/metadata'
-	params = {'showOnlyActive':'true','dataType':framework}
+	url = 'https://dataland.com/api/metadata'
+	params = {'showOnlyActive': 'true', 'dataType': framework}
 	resp = requests.get(url=url, headers=headers, params=params).json()
-	print("Framework "+framework+" has "+str(len(resp))+" active datasets.")
+	datasetCountPerFramework[framework] = extractNumnberOfDatasetsPerReportingPeriod(resp)
+	print("Framework " + framework + " has " + str(datasetCountPerFramework[framework]) + " dataset combinations for a "
+																						  "total of " + str(
+		len(resp)) + " active datasets.")
--- users_number_script.py
+++ users_number_script.py
@@ -1,16 +1,38 @@
 # Script requires port 6789 from the dataland server to be forwarded to the executing machine
 
-from requests.auth import HTTPBasicAuth
 import requests
 
 from config import *
 
-token_url='http://dataland-admin:6789/keycloak/realms/master/protocol/openid-connect/token'
-body={'grant_type':'password','username':KEYCLOAK_USERNAME,'password':KEYCLOAK_PASSWORD,'client_id':'admin-cli','client_secret':''}
+token_url = 'http://dataland-admin:6789/keycloak/realms/master/protocol/openid-connect/token'
+body = {'grant_type': 'password', 'username': KEYCLOAK_USERNAME, 'password': KEYCLOAK_PASSWORD,
+        'client_id': 'admin-cli', 'client_secret': ''}
 token_resp = requests.post(url=token_url, data=body).json()
 token = token_resp['access_token']
-url='http://dataland-admin:6789/keycloak/admin/realms/datalandsecurity/users/'
-headers = {"Authorization": "Bearer "+token}
-params = {"Enabled":"true", "emailVerified":"true", "max":"1000"}
-resp = requests.get(url=url,headers=headers,params=params).json()
-print("We have "+str(len(resp))+" enabled and e-mail verified users on Dataland")
+url = 'http://dataland-admin:6789/keycloak/admin/realms/datalandsecurity/users/'
+headers = {"Authorization": "Bearer " + token}
+params1 = {"Enabled": "true", "emailVerified": "false", "max": "10000"}
+params2 = {"Enabled": "true", "emailVerified": "true", "max": "10000"}
+params3 = {"Enabled": "true", "max": "10000"}
+
+UnverifiedUsers = requests.get(url=url, headers=headers, params=params1).json()  # This request also lists 3 technical
+# users which should not be included in the analytics
+verifiedUsers = requests.get(url=url, headers=headers, params=params2).json()
+totalUsers = requests.get(url=url, headers=headers, params=params3).json()  # This request does not include the 3
+# technical users for unknown reasons. Anyway, this is the desired number
+
+unverifiedIDs = [entry['id'] for entry in UnverifiedUsers]
+verifiedIDs = [entry['id'] for entry in verifiedUsers]
+totalIDs = [entry['id'] for entry in totalUsers]
+technicalUsers = set(unverifiedIDs) - set(totalIDs)
+# print(len(technicalUsers))
+# print(technicalUsers)
+
+for entry in technicalUsers:
+    pass
+    # print("duplicate index: " + str(list(unverifiedIDs).index(entry)))
+
+numberOfTechnicalUsers = 3
+print("We have " + str(len(UnverifiedUsers) - numberOfTechnicalUsers) + " enabled, but unverified users on Dataland")
+print("We have " + str(len(verifiedUsers)) + " enabled and e-mail verified users on Dataland")
+print("We have " + str(len(totalUsers)) + " total signed up users on Dataland")
