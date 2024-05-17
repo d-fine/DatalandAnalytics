191865dddf18138026cefe5fd9b9d04e94355aef
Andreas Hoecherl <andreas.hoecherl@d-fine.com>
2024-05-17 16:19:56 +0200

    Add time filter possibility

--- dataland_analytics/analysis_scripts/scripts_with_api_admin/requests_by_specific_user.py
+++ dataland_analytics/analysis_scripts/scripts_with_api_admin/requests_by_specific_user.py
@@ -3,8 +3,11 @@ import dataland_requests
 import dataland_datasets
 from dataland_analytics.config import *
 import pandas as pd
+import time
 
 user_id = ""
+start_epoch = 1715910891000
+end_epoch = round(time.time() * 1000)
 
 datasets_configuration = dataland_datasets.Configuration(
     host="https://dataland.com/api", access_token=PERSONAL_USER_API_KEY
@@ -19,7 +22,12 @@ result_dicts = []
 with dataland_requests.ApiClient(requests_configuration) as api_client:
     api_instance = dataland_requests.RequestControllerApi(api_client)
     request_list = api_instance.get_data_requests(user_id=user_id)
-    asdf = 1
+    if "start_epoch" in locals():
+        request_list = [
+            r
+            for r in request_list
+            if (r.creation_timestamp > start_epoch and r.creation_timestamp < end_epoch)
+        ]
 
 with dataland_datasets.ApiClient(datasets_configuration) as api_client:
     company_api_instance = dataland_datasets.CompanyDataControllerApi(
@@ -39,11 +47,15 @@ with dataland_datasets.ApiClient(datasets_configuration) as api_client:
             reporting_period=request.reporting_period,
         )
         exists = True if matching_dataset else False
-        result_dicts.append({"Company_name":company.company_information.company_name,
-                             "LEI":company.company_information.identifiers['Lei'][0],
-                             "Framework":request.data_type,
-                             "Year":request.reporting_period,
-                             "Exists":exists})
+        result_dicts.append(
+            {
+                "Company_name": company.company_information.company_name,
+                "LEI": company.company_information.identifiers["Lei"][0],
+                "Framework": request.data_type,
+                "Year": request.reporting_period,
+                "Exists": exists,
+            }
+        )
 
 result = pd.DataFrame.from_records(result_dicts)
-result.to_csv("specific_user_requests.csv",index=False)
+result.to_csv("specific_user_requests.csv", index=False)
