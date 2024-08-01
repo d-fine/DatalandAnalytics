d7a733d69da6ecd95f5ace820bf7a1c1eb17a280
Dennis Groh <dennis.groh@d-fine.com>
2024-08-01 11:20:52 +0200

    Update openApi yamls and fix PEP8 issues

--- dataland_analytics/analysis_scripts/scripts_with_api_read/datasets_per_framework_script.py
+++ dataland_analytics/analysis_scripts/scripts_with_api_read/datasets_per_framework_script.py
@@ -7,19 +7,20 @@ pd.options.display.max_columns = 20
 pd.options.display.max_rows = 20
 
 configuration = dataland_datasets.Configuration(
-	host = DATALAND_SERVER + "api",
-	access_token = PERSONAL_USER_API_KEY
+    host=DATALAND_SERVER + "api",
+    access_token=PERSONAL_USER_API_KEY
 )
 
-frameworks = ['sme', 'sfdr', 'p2p', 'lksg', 'heimathafen', 'eutaxonomy-non-financials', 'esg-questionnaire',
-			  'eutaxonomy-financials']
-reporting_periods = ['2020','2021','2022','2023','2024']
+frameworks = ['vsme', 'sfdr', 'p2p', 'lksg', 'heimathafen', 'eutaxonomy-non-financials', 'esg-questionnaire',
+              'eutaxonomy-financials']
+reporting_periods = ['2020', '2021', '2022', '2023', '2024']
 result = pd.DataFrame(columns=frameworks, index=reporting_periods)
 
 with dataland_datasets.ApiClient(configuration) as api_client:
-	api_instance = dataland_datasets.MetaDataControllerApi(api_client)
-	for (framework, year) in list(itertools.product(frameworks,reporting_periods)):
-		list_of_meta_data_info = api_instance.get_list_of_data_meta_info(show_only_active=True, data_type=framework, reporting_period=year)
-		result.loc[year,framework]=len(list_of_meta_data_info)
-		
+    api_instance = dataland_datasets.MetaDataControllerApi(api_client)
+    for (framework, year) in list(itertools.product(frameworks, reporting_periods)):
+        list_of_meta_data_info = api_instance.get_list_of_data_meta_info(show_only_active=True, data_type=framework,
+                                                                         reporting_period=year)
+        result.loc[year, framework] = len(list_of_meta_data_info)
+
 print(result)
